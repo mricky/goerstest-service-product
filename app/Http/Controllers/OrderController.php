35b34461c88e589705c5f541e7bff8ad5f1fb1bd
Mohammad Ricky <mricky.it@gmail.com>
2022-08-25 10:23:41 +0700

    checkout

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -4,22 +4,51 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Helpers\Formater;
+use App\Models\Order;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Validator;
-
+use PhpParser\Node\Stmt\Catch_;
+use Illuminate\Support\Str;
 class OrderController extends Controller
 {
    
     public function store(Request $request){
-       $data = $request->only('order_date','qty','user_id');
+       
+     $data = $request->only('order_date','user_id','order_number','total');
      
        $validator = Validator::make($data,[
             'order_date' => 'required|date',
             'user_id'   => 'nullable',
+            'status' => 'required|in:pending,success,close',
             'items' => 'nullable|array',
             'items.*.product_id' => 'required',
             'items.*.qty' => 'required'
        ]);
+
+       try {
+          DB::beginTransaction();
+          $order = Order::create($data);
+    
+          $items = $request->input('items',[]);
+          $items = collect($items)->transform(function($item) use ($order){
+            
+               $item['order_id'] = $order->id;
+               $item['product_id']  = $item['product_id'];
+               $item['qty'] = $item['qty'];
+             
+               return $item;
+          });
+     
+          $order->orderItems()->createMany($items);
+
+          $msg = 'Order was succefully created';
+          DB::commit();
+          return Formater::success($order, $msg);
+       } Catch(\Exception $e) {
+         
+          DB::rollBack();
+          return Formater::error($e->getMessage(),500);
+       }
 
        if($validator->fails()){
             return Formater::error(
